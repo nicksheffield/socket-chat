5402ed0e3f5e73e391ae25df712c1d5f7fdd5b0d
Nick Sheffield <numbereft@gmail.com>
2015-09-10 14:05:35 +1200

    updated comments and use io.listen()

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-var io = require('socket.io')(9999)
+var io = require('socket.io')()
 
+// start socket.io on port 9999
+io.listen(9999)
+
+// when a client connects to the socket.io server
 io.on('connection', function(socket) {
 	console.log('connection from', socket.id)
 	
+	// send them a welcome message
 	socket.emit('welcome', 'Yo waddup!')
 	
 	// when we receive the message from the client
